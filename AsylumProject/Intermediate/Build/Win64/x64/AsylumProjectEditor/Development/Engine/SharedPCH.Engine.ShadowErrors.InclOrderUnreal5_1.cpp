bd5dccbabba47308fff6fef326fc02bd3af225a8
Héctor de Anda <hdeandaf@hotmail.com>
2023-10-19 23:54:09 -0600

    remove files for gitignore

--- AsylumProject/Intermediate/Build/Win64/x64/AsylumProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.InclOrderUnreal5_1.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.37.32822
-#include "E:/Proyects/GitHub/UE 5/Asylum/AsylumProject/Intermediate/Build/Win64/x64/AsylumProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.InclOrderUnreal5_1.h"
--- AsylumProject/Source/AsylumProject/AsylumProject.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "AsylumProject.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, AsylumProject, "AsylumProject" );
- 
